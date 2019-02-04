1c4dc9ada5e38f05e20157d5fdfdb5f7e97435e6
Kyler <Krenneker16@gmail.com>
2019-02-04 17:19:12 -0500

    added nav link scrolling functionality using Jquery

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 $(function(){
-
+    $(".header").find("a").click(function(e) {
+        e.preventDefault();
+        var section = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(section).offset().top}, 'slow');
+    });
 })
