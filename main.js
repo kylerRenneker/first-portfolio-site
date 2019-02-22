958bdeeff5c86ac591c66c083c28fa83625e7804
Kyler <Krenneker16@gmail.com>
2019-02-21 22:46:07 -0500

    modified landing page and header

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(function(){
-    $(".header").find("a").click(function(e) {
+    $("header").find("a").click(function(e) {
         e.preventDefault();
         var section = $(this).attr("href");
         $("html, body").animate({
@@ -11,12 +11,12 @@ $(window).scroll(function(){
     console.log('scroll');
     if($(this).scrollTop() > 40){
         $('.arrow').addClass('hide');
-        $('.header').addClass('active');
+        $('header').addClass('active');
         $('.nav-links a').addClass('link-black');
     }
     else {
         $('.arrow').removeClass('hide');
-        $('.header').removeClass('active');
+        $('header').removeClass('active');
         $('.nav-links a').removeClass('link-black');
     }
 })
