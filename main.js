599351eeff7c3dad48e9b1a3f3bad364769e01e3
Kyler <Krenneker16@gmail.com>
2019-02-24 21:16:01 -0500

    changed font sizes

--- main.js
+++ main.js
@@ -5,24 +5,8 @@ $(function(){
         $("html, body").animate({
             scrollTop: $(section).offset().top}, 'slow');
     });
-
-
-// $(window).scroll(function(){
-//     console.log('scroll');
-//     if($(this).scrollTop() > 80){
-//         $('.arrow').addClass('hide');
-//         $('header').addClass('active');
-//         $('.nav-links a').addClass('link-black');
-//     }
-//     else {
-//         $('.arrow').removeClass('hide');
-//         $('header').removeClass('active');
-//         $('.nav-links a').removeClass('link-black');
-//     }
-// })
-
+    
 $(window).on('scroll', _.debounce(function() {
-    // Do expensive things
     console.log(scroll);
     if($(this).scrollTop() > 60){
         // $('.arrow').addClass('hide');
