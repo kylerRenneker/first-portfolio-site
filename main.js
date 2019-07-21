8753afe4d0e622796fb8a346a23a54c1ea8b6726
Kyler <Krenneker16@gmail.com>
2019-07-21 01:22:35 -0400

    still need to add project image for Handyman

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-$(function(){
-    $("header").find("a").click(function(e) {
+$(function headerClickScroll() {
+    $("header").find("a").click(function (e) {
         e.preventDefault();
+        console.log(this)
         var section = $(this).attr("href");
         $("html, body").animate({
-            scrollTop: $(section).offset().top}, 'slow');
+            scrollTop: $(section).offset().top
+        }, 'slow');
     });
-    
-$(window).on('scroll', _.debounce(function() {
-    console.log(scroll);
-    if($(this).scrollTop() > 60){
-        // $('.arrow').addClass('hide');
-        $('header').addClass('active');
-        $('.nav-links a').addClass('link-black');
-    }
-    else {
-        // $('.arrow').removeClass('hide');
-        $('header').removeClass('active');
-        $('.nav-links a').removeClass('link-black');
-    }
-  }, 100));
+
+    $(window).on('scroll', _.debounce(function () {
+        if ($(this).scrollTop() > 60) {
+            $('header').addClass('active');
+            $('.nav-links a').addClass('link-black');
+        }
+        else {
+            $('header').removeClass('active');
+            $('.nav-links a').removeClass('link-black');
+        }
+    }, 100));
 
 })
+
+// $(function setSkills() {
+//     let skills = ['HTML', 'CSS', 'JavaScript']
+//     return skills.map(skill => {
+//         return $('.landing-skills').append(`<li>${skill}<span class="landing-skill-list">•</span></li>`)
+//     })
+// })
